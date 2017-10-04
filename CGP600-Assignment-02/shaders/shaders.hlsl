5e00c229d7e4943b68719d27ae09c860bf6f3b55
Julian <julzso23@gmail.com>
2017-10-04 19:13:09 +0100

    Added cbuffer to shader

--- CGP600-Assignment-02/shaders/shaders.hlsl
+++ CGP600-Assignment-02/shaders/shaders.hlsl
@@ -1,3 +1,9 @@
+cbuffer CBuffer0
+{
+    float redFraction;
+    float3 packing;
+};
+
 struct VOut
 {
     float4 position : SV_POSITION;
@@ -7,6 +13,7 @@ struct VOut
 VOut VShader(float4 position : POSITION, float4 color : COLOR)
 {
     VOut output;
+    color.r *= redFraction;
     output.position = position;
     output.color = color;
     return output;
