b252ded36167851a1995d4cb0e7bc970d4cfd311
Julian <julzso23@gmail.com>
2017-11-12 20:01:17 +0000

    Enabled anisotropic filtering

--- CGP600-Assignment-02/src/Window.cpp
+++ CGP600-Assignment-02/src/Window.cpp
@@ -255,7 +255,7 @@ HRESULT Window::initialiseGraphics()
 
     D3D11_SAMPLER_DESC samplerDescription;
     ZeroMemory(&samplerDescription, sizeof(samplerDescription));
-    samplerDescription.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+    samplerDescription.Filter = D3D11_FILTER_ANISOTROPIC;
     samplerDescription.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
     samplerDescription.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
     samplerDescription.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
